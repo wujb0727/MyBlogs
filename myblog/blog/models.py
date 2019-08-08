1cbb96723370fa79dd52179106f6580f808dcb01
MyGit <example@453547966@qq.com>
2019-08-08 08:25:22 +0800

    Initial commit

---
+++ myblog/blog/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import User  # 导入User表
+
+
+# Create your models here.
+
+# 博客分类表
+class BlogType(models.Model):
+    type_name = models.CharField(max_length=15)  # 博客分类名称
+
+    def __str__(self):  # 显示标签名
+        return self.type_name
+
+
+# 博客表
+class Blog(models.Model):
+    title = models.CharField(max_length=50)  # 博客标题
+    blog_type = models.ForeignKey(BlogType, on_delete=models.DO_NOTHING)  # 博客分类
+    content = models.TextField()  # 博客内容
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING)  # 博客作者
+    created_time = models.DateTimeField(auto_now_add=True)  # 创建时间
+    last_update_time = models.DateTimeField(auto_now=True)  # 博客更新时间
+
+    def __str__(self):  # 显示标题名
+        return "<Blog:{}>".format(self.title)
+
+    class Meta:
+        ordering = ['-created_time']  # 定义排序规则，按照创建时间倒序
---
+++ myblog/myblog/__init__.py
@@ -0,0 +1,4 @@
+# 将mysql当做默认数据库
+import pymysql
+
+pymysql.install_as_MySQLdb()
